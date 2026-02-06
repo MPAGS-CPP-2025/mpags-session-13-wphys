a8cfe73f4bba15e466eb6662ed45b88b6dfa9cb1
Vlad-Ioan Tir <vxt578@student.bham.ac.uk>
2026-02-06 11:45:51 +0000

    finish exercise

--- src/MPAGSCipher/PlayfairCipher.cpp
+++ src/MPAGSCipher/PlayfairCipher.cpp
@@ -1,8 +1,8 @@
 #include "PlayfairCipher.hpp"
 
 #include <algorithm>
+#include <iostream>
 #include <string>
-
 /**
  * \file PlayfairCipher.cpp
  * \brief Contains the implementation of the PlayfairCipher class
@@ -62,26 +62,79 @@ void PlayfairCipher::setKey(const std::string& key)
 }
 
 std::string PlayfairCipher::applyCipher(const std::string& inputText,
-                                        const CipherMode /*cipherMode*/) const
+                                        const CipherMode cipherMode) const
 {
     // Create the output string, initially a copy of the input text
     std::string outputText{inputText};
 
-    // Change J -> I
-
-    // Find repeated characters and add an X (or a Q for repeated X's)
-
-    // If the size of the input is odd, add a trailing Z
-
-    // Loop over the input bigrams
-
-    // - Find the coordinates in the grid for each bigram
-
-    // - Apply the rules to these coords to get new coords
-
-    // - Find the letters associated with the new coords
-
-    // - Make the replacements
+    if (cipherMode == CipherMode::Encrypt) {
+        // Change J -> I
+        std::transform(std::begin(outputText), std::end(outputText),
+                       std::begin(outputText),
+                       [](char c) { return (c == 'J') ? 'I' : c; });
+        // Find repeated characters and add an X (or a Q for repeated X's)
+        for (std::size_t i{0}; i < outputText.size() - 1; i += 2) {
+            if (outputText[i] == outputText[i + 1]) {
+                char insertChar = (outputText[i] == 'X') ? 'Q' : 'X';
+                outputText.insert(i + 1, 1, insertChar);
+            }
+        }
+        // If the size of the input is odd, add a trailing Z
+        if (outputText.size() % 2 != 0) {
+            outputText += (outputText.back() == 'Z') ? 'X' : 'Z';
+        }
+        // Loop over the input bigrams
+        for (std::size_t i{0}; i < outputText.size() - 1; i += 2) {
+            // - Find the coordinates in the grid for each bigram
+            auto [row1, column1] = charLookup_.at(outputText[i]);
+            auto [row2, column2] = charLookup_.at(outputText[i + 1]);
+            // - Apply the rules to these coords to get new coords
+            if (row1 == row2) {
+                // Same row: shift columns to the right
+                column1 = (column1 + 1) % gridSize_;
+                column2 = (column2 + 1) % gridSize_;
+            } else if (column1 == column2) {
+                // Same column: shift rows down
+                row1 = (row1 + 1) % gridSize_;
+                row2 = (row2 + 1) % gridSize_;
+            } else {
+                // Rectangle: swap columns
+                std::swap(column1, column2);
+            }
+            // - Find the letters associated with the new coords
+            char newChar1 = coordLookup_.at({row1, column1});
+            char newChar2 = coordLookup_.at({row2, column2});
+            // - Make the replacements
+            outputText[i] = newChar1;
+            outputText[i + 1] = newChar2;
+        }
+        
+    } else {
+        for (std::size_t i{0}; i < outputText.size() - 1; i += 2) {
+            // - Find the coordinates in the grid for each bigram
+            auto [row1, column1] = charLookup_.at(outputText[i]);
+            auto [row2, column2] = charLookup_.at(outputText[i + 1]);
+            // - Apply the rules to these coords to get new coords
+            if (row1 == row2) {
+                // Same row: shift columns to the right
+                column1 = (gridSize_ + column1 - 1) % gridSize_;
+                column2 = (gridSize_ + column2 - 1) % gridSize_;
+            } else if (column1 == column2) {
+                // Same column: shift rows down
+                row1 = (gridSize_ + row1 - 1) % gridSize_;
+                row2 = (gridSize_ + row2 - 1) % gridSize_;
+            } else {
+                // Rectangle: swap columns
+                std::swap(column1, column2);
+            }
+            // - Find the letters associated with the new coords
+            char newChar1 = coordLookup_.at({row1, column1});
+            char newChar2 = coordLookup_.at({row2, column2});
+            // - Make the replacements
+            outputText[i] = newChar1;
+            outputText[i + 1] = newChar2;
+        }
+    }
 
     // Return the output text
     return outputText;
